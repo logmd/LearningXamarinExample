0e146939686b4fc6afaaf20370f021c317afb804
amrik <amrik@LAPTOP-DD95DR2A>
2016-11-27 17:33:46 +0000

    add global app.xaml and .cs
    add global colors

---
+++ GuitarShopApp/GuitarShop/GuitarShop/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using GuitarShop.Layouts;
+using Xamarin.Forms;
+
+namespace GuitarShop
+{
+    public partial class App: global::Xamarin.Forms.Application
+    {
+        public App()
+        {
+            // The root page of your application
+            MainPage = new MainPage();
+            InitializeComponent();
+        }
+
+        //protected override void OnStart()
+        //{
+        //    // Handle when your app starts
+        //}
+
+        //protected override void OnSleep()
+        //{
+        //    // Handle when your app sleeps
+        //}
+
+        //protected override void OnResume()
+        //{
+        //    // Handle when your app resumes
+        //}
+    }
+}
