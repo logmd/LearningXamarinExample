38cd1d7ee0171c7fbcfb71c60dacf6c2be00ff9f
asovde <asovde@hotmail.com>
2016-12-05 21:20:58 +0000

    Added azure mobile services package

--- GuitarShopApp/GuitarShop/GuitarShop/Service/ThirdParty/InventoryFacade.cs
+++ GuitarShopApp/GuitarShop/GuitarShop/Service/ThirdParty/InventoryFacade.cs
@@ -1,53 +1,22 @@
-﻿using GuitarShop.BusinessObjects;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.IO;
 using System.Text;
 using System.Threading.Tasks;
-using Xamarin.Forms;
+using GuitarShop.BusinessObjects;
 
 namespace GuitarShop.Service.ThirdParty
 {
     public class InventoryFacade : IInventoryFacade
     {
-        public IInventoryService _service;
-
-        public InventoryFacade(IInventoryService service)
+        public IEnumerable<Product> GetAllProducts()
         {
-            _service = service;
+            throw new NotImplementedException();
         }
 
         public Product GetProductByProductNumber(int productNumber)
         {
-            var item = _service.GetById(productNumber);
-
-            return new Product
-            {
-                ProductNumber = item.IventoryId,
-                ProductName = item.Name,
-                Price = item.Price,
-                Quantity = item.Quantity,
-                Description = item.Description,
-                Features = item.Features,
-                ImageUrl = item.ImageUrl
-            };
-        }
-
-        public IEnumerable<Product> GetAllProducts()
-        {
-            return _service.GetAll()
-                .Select(i => new Product
-                {
-                    ProductNumber = i.IventoryId,
-                    ProductName = i.Name,
-                    Price = i.Price,
-                    Quantity = i.Quantity,
-                    Description = i.Description,
-                    Features = i.Features,
-                    ImageUrl = i.ImageUrl
-                });
+            throw new NotImplementedException();
         }
     }
 }
