e243853268cf0f69e9b4f6dc91abbfefc109c6b1
asovde <asovde@hotmail.com>
2017-01-12 19:29:22 +0000

    Project setup update

--- GuitarShopApp/MobileService/ScheduledJobs/SampleJob.cs
+++ GuitarShopApp/MobileService/ScheduledJobs/SampleJob.cs
@@ -6,12 +6,12 @@ namespace MobileService.ScheduledJobs
     // A simple scheduled job which can be invoked manually by submitting an HTTP
     // POST request to the path "/jobs/sample".
 
-    public class SampleJob : ScheduledJob
+    public class SampleJob //: ScheduledJob
     {
-        public override Task ExecuteAsync()
-        {
-            Services.Log.Info("Hello from scheduled job!");
-            return Task.FromResult(true);
-        }
+        //public override Task ExecuteAsync()
+        //{
+        //    Services.Log.Info("Hello from scheduled job!");
+        //    return Task.FromResult(true);
+        //}
     }
 }
