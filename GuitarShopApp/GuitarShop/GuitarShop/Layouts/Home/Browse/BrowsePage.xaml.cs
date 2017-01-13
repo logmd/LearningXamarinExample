797da48124699fb4d9fe20bec9ce3e0d732cbf5b
abhogal-kpmg <amrik.bhogal@kpmg.co.uk>
2017-01-13 01:30:04 +0000

    add browse page

---
+++ GuitarShopApp/GuitarShop/GuitarShop/Layouts/Home/Browse/BrowsePage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using GuitarShop.BusinessObjects;
+using GuitarShop.IoC;
+using GuitarShop.Layouts.Templates;
+using GuitarShop.Service.DataContracts.DTO;
+using GuitarShop.Service.DataContracts.Requests;
+using GuitarShop.Service.ServiceContract;
+using Xamarin.Forms;
+
+namespace GuitarShop.Layouts.Home.Browse
+{
+    public partial class BrowsePage : DetailedContentPage
+    {
+        public BrowsePage()
+        {
+            
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            try
+            {
+
+                AppSetup setup = DependencyService.Get<AppSetup>();
+
+                var service = setup.Container.Resolve<IProductService>();
+
+                var getProductRequest = new GetAllProductsRequest();
+
+                var result = await service.GetAllProducts(getProductRequest);
+
+                var products = result?.Products;
+
+                var listInView = AllProductsListView;
+
+                var lview = new ListView();
+                
+                var source = new ObservableCollection<ListProductDTO>();
+
+                foreach (var p in products)
+                {
+                    source.Add(p);
+                }
+
+                AllProductsListView.ItemsSource = source;
+
+            }
+            catch (Exception e)
+            {
+            }
+        }
+    }
+
+    public class ProductViewModel
+    {
+        public string Name { get; set; }
+    }
+}
